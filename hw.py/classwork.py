65b504221e3f2b03c652326116d644d75ad9f257
unknown <hk8704394@gmail.com>
2025-07-10 13:55:21 +0545

    code added

---
+++ hw.py/classwork.py
@@ -0,0 +1,28 @@
+writr a program to detect double spaces in a string.
+text = input ("enter a string ")
+if " " in text:
+    print("double space detected")
+else:
+    print("No double space found")
+
+
+
+write a program to format the following letter using escape sequence character
+#letter ="Dear Students, This python course is going well . Thank you"
+letter = "Dear Students,\nThis python course is going well.\nThank you"
+print(letter)
+
+#write a program to detect double space in a string
+text = input ("enter a string ")
+if " " in text:
+    print("double space detected")
+else:
+    print("No double space found")
+
+
+
+#replace the double space from problem 3 with single spaces
+
+text=input("enter a string")
+fixed_text=text.replace(" "," ")
+print("fixed string:",fixed_text)
